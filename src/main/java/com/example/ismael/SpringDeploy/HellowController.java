3f511df52cc0c86d6ca9bfea8a7784bdacf1939f
Ismael Quinta <ismael.quinta.dev@gmail.com>
2022-05-18 02:12:09 +0200

    spring heroku app config

---
+++ src/main/java/com/example/ismael/SpringDeploy/HellowController.java
@@ -0,0 +1,25 @@
+package com.example.ismael.SpringDeploy;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellowController {
+
+
+  //  @Value("${app.varexample}")
+    @Value("${app.message}")
+    String message;
+
+
+
+    @GetMapping("/hola")
+    public String holaMundo(){
+
+        System.out.println(message);
+        return "Hola mundo !!! Adios mundo!!!";
+    }
+
+}
